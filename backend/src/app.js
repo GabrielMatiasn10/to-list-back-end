f55cd9111c2236ea393442fb349c404d4cd7222e
GabrielMatiasn10 <Gm4037003@gmail.com>
2023-05-07 14:24:51 -0300

    add cors

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,9 +1,10 @@
 const express =require('express');
 const router = require('./router');
-
+const cors = require('cors')
 const app = express();
 
 app.use(express.json());
+app.use(cors())
 app.use(router);
 
 app.get('/', (request, response) => response.status(200).send ('hello, oi'));
